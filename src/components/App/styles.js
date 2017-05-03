388bffbfbc53f81e48d886a6bc8897584d6eb0b4
kitze <kristijan.mkd@gmail.com>
2017-05-03 14:15:08 +0200

    🛠 remove "fixed" from toolbar, move styles and toolbar to App

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
+import flex from 'styles/flex';
+
+import {colorTransition} from 'styles/shared';
 
 export const App = styled.div`
-  
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: ${p => p.theme.backgroundColor};
+  transition: ${colorTransition};
+  ${flex.vertical}
 `;

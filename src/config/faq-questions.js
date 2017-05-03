ca029d3c8874b5f93daa22680a0252ddb8993b28
kitze <kristijan.mkd@gmail.com>
2017-05-03 19:43:29 +0200

    🛠 faq: minor changes, add rename-index npm script for netlify

--- src/config/faq-questions.js
+++ src/config/faq-questions.js
@@ -47,9 +47,9 @@ const questions = [
     title: `Why isn't the scroll synchronized between devices?`,
     answer: (
       <div>
-        For now, it doesn't make sense because there's mix of various devices and sizes which can be in either portrait on landscape.
-        So if you imagine a situation where you have a huge iPad and a small iPhone next to it and the scroll is synchronized, with one scroll on the iPhone you're going to reach the end of the site on the iPad.
-        So the scroll won't really be "synced". This feature won't be implemented until there's a proper solution so the scroll is not actually "synced" but it's focusing on the same portion of the page on all devices.
+        For now, it doesn't make sense because there's a mix of various devices and sizes which can either be in portrait on landscape.
+        Imagine a situation where you have a huge iPad and a small iPhone next to it and the scroll is synchronized, with one scroll on the iPhone, you're going to reach the end of the site on the iPad.
+        So the scroll won't really be "synced". This feature won't be implemented until there's a proper solution where the scroll is not actually "synced", but it's focusing on the same portion of the page on all devices.
       </div>
     )
   },
@@ -81,7 +81,7 @@ const questions = [
       <div>
         Yes you can.
 
-        If you're running a local server, just paste http://localhost:3000 or whatever the link of your local webserver is into the url bar of Sizzy.
+        If you're running a local server, just paste http://localhost:3000, or whatever the link of your local webserver is, into the url bar of Sizzy.
         Keep in mind that the link must start with https:// or http://.
         You cannot point to an index.html file that's starting with file://.
         If you need a simple server to run your local files you can use
@@ -90,7 +90,7 @@ const questions = [
           http-server
         </a>
         {' '}
-        (a zero-configuration command-line http server). constructor
+        (a zero-configuration command-line http server).
         Just start http-server, point it to the folder of your website and it will serve your app on http://localhost:8080. Then you can paste http://localhost:8080 in the url bar of Sizzy.
       </div>
     )
@@ -125,15 +125,15 @@ const questions = [
         >
           open sourced
         </a>
-        , so you freely explore what it does.
+        , so you can explore it and see what it does.
 
         The extension has 2 basic functionalities:
         <ol>
           <li>
-            When clicking the "Sizzy" button, it will open the current website in Sizzy.
+            When clicking the "S" button in the toolbar, it opens the current website in Sizzy.
           </li>
           <li>
-            It blocks "x-frame-options" headers from the requests, so you can embed most of the websites that prevent embedding. any site you want.
+            It blocks "x-frame-options" headers from the requests, so you can embed most of the websites that prevent embedding.
             {' '}
           </li>
         </ol>
@@ -149,7 +149,7 @@ const questions = [
         {' '}
         {`"[ "activeTab", "webRequest", "webRequestBlocking", "<all_urls>" ]`}
 
-        I would understand your concert if the extension had a closed source, but you have nothing to worry about.
+        I would understand your concern if the extension had a closed source, but in this case you have nothing to worry about.
         If you're not very technical and you need a further explanation, feel free to contact me anywhere!
       </div>
     )
@@ -159,17 +159,17 @@ const questions = [
     title: `I really like Sizzy! How can I make sure that it will stick around and be updated with awesome new features?`,
     answer: (
       <div>
-        Currently, the project is mostly being developed
+        Currently, this project is being developed mostly
         {' '}
         <a href="https://kitze.io">by 1 person</a>
         .
-        Development will continue as usual but it would help greatly if you can help and support this project with a one time or a recurring donation
+        Development will continue as usual but it would help greatly if you support this project with a one-time, or a recurring donation
         {' '}
         <a target="_blank" href="https://opencollective.com/sizzy">
           {' '}on OpenCollective
         </a>
-        . You can also become a sponsor of the app!
-        If you would like to discuss a different way of supporting Sizzy, you can
+        .
+        If you would like to discuss about a different way of supporting Sizzy, you can
         {' '}
         <a target="_blank" href="https://kitze.io"> contact me here</a>
         .

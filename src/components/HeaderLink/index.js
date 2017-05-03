2650a618914b252535f475bd0e7ac09c7d9fd22f
kitze <kristijan.mkd@gmail.com>
2017-05-03 14:12:30 +0200

    🛠 modify HeaderLink to accept router views

--- src/components/HeaderLink/index.js
+++ src/components/HeaderLink/index.js
@@ -2,19 +2,30 @@
 import React, {Component} from 'react';
 
 //styled-components
-import {HeaderLink, Icon, Text} from './styles';
+import {HeaderLink, Icon, Text, RouterLink} from './styles';
 
 type Props = {
   icon: string,
   text: string,
-  link: string
+  link: ?string,
+  view: ?Object,
+  router: ?Object
 };
 
 class HeaderLinkComponent extends Component {
   props: Props;
 
   render() {
-    const {icon, text, link} = this.props;
+    const {icon, text, router, view, link} = this.props;
+
+    if (view) {
+      return (
+        <RouterLink view={view} router={router}>
+          <Icon name={icon} />
+          <Text> {text} </Text>
+        </RouterLink>
+      );
+    }
 
     return (
       <HeaderLink href={link} target="_blank">

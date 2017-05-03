72bf597808d551f8af7ead4dad9809e5dbb863a5
kitze <kristijan.mkd@gmail.com>
2017-05-03 14:33:08 +0200

    🖌 add style for faq questions and selected question

---
+++ src/config/faq-questions.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+const questions = [
+  {
+    id: 'where-can-i-find-the-code',
+    title: 'Where can I find the code?',
+    answer: (
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </div>
+    )
+  },
+  {
+    id: 'who-made-this',
+    title: 'Who made this?',
+    answer: (
+      <div>
+        I made this. I.
+      </div>
+    )
+  },
+  {
+    id: 'is-there-a-chrome-extension',
+    title: 'Is there a chrome extension?',
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-4',
+    title: 'Is there a chrome extension?',
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-5',
+    title: 'Is there a chrome extension?',
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-6',
+    title: 'Is there a chrome extension?',
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-7',
+    title: ``,
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-8',
+    title: `What's going on world?`,
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  },
+  {
+    id: 'question-9',
+    title: 'Hello world?',
+    answer: (
+      <div>
+        Yup. If you looked around a little bit, you would've seen that it's in the header.
+      </div>
+    )
+  }
+];
+
+export default questions;
--- src/views/FaqView/styles.js
+++ src/views/FaqView/styles.js
@@ -1,5 +1,46 @@
 import styled from 'styled-components';
+import {toolbarHeight} from 'styles/sizes';
+import {cond} from 'utils/sc-utils';
 
 export const FaqView = styled.div`
+  color: white;
+  padding: 0 30px 30px 30px;
+  width: 100vw;
+  height: calc(100vh - ${toolbarHeight}px);
+  overflow-y: scroll;
+`;
+
+export const PageTitle = styled.div`
+  font-size: 30px;
+  color: white;
+  font-weight: 300;
+  margin: 50px 0;
+`;
+
+export const Questions = styled.div`
+  max-width: 700px;
+`;
+
+export const Question = styled.div`
+  margin-bottom: 20px;
+  padding: 17px 20px;
+  border: 1px solid rgba(241, 241, 241, 0.1);
   
+  ${p => cond(p.current, `
+    border: 1px solid rgba(241, 241, 241, 0.57);
+    background-color: rgba(34, 39, 47, 0.8);
+  `)}
+`;
+
+export const QuestionTitle = styled.div`
+  font-size: 23px;
+  font-weight: 300;
+  margin-bottom: 5px;
+`;
+
+export const Answer = styled.div`
+  font-size: 18px;
+  font-weight: 100;
+  color: #bbbbbb;
+  line-height: 23px;
 `;
